5e3c969ec2dc7ee763484dbd6c0ded97f4336a6f
Shankar Dutta <shankardutta1719@gmail.com>
2025-12-03 21:58:14 +0530

    Initialize App Routing Structure

---
+++ src/app/addstudent/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Add Student | Student Management CRUD App",
+	description:
+		"Add Student page for the Student Management CRUD system built with Next.js. Use the simple and responsive form to enter and save new student details efficiently, ensuring smooth and organized student data management.",
+};
+
+const page = () => {
+	return <div>Add Students</div>;
+};
+
+export default page;
---
+++ src/app/updatedetails/student/[stuid]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Update Student Details | Student Management CRUD App",
+	description:
+		"Update Student page in the Next.js Student Management CRUD system. Modify and save existing student information through a smooth, responsive form designed for efficient data management.",
+};
+const page = () => {
+	return <div>Update Student Details</div>;
+};
+
+export default page;
